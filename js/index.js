71d373e6aaf15d9f39a8ed5c2eeedf0d19fdada4
ChadDiaz <chadcdiaz@gmail.com>
2020-08-30 18:20:28 -0400

    3 of 10 MVP done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+const log=console.log;
+
+// MOUSEOVER SECTION
+const logoWork = document.querySelector(".logo-heading")
+logoWork.addEventListener("mouseover" , () =>{
+logoWork.style.transform = "scale(1.4)";
+logoWork.style.tranistion = "all 0.3s";
+logoWork.style.color = "lightblue";
+});
+logoWork.addEventListener("mouseleave", (event) => {
+    event.target.style.transform = "scale(1.0)";
+    event.target.style.color = "black";
+});
+
+// DOUBLE CLICK SECTION 
+const navChange = document.querySelector(".nav")
+console.log(navChange)
+navChange.addEventListener("dblclick", (event)=>{
+    console.log(event.target)
+    event.target.style.color="pink";
+})
+
+// KEYDOWN SECTION
+const entireBody = document.querySelector("body")
+log(entireBody)
+entireBody.addEventListener("keydown", (key)=>{
+    if(key.keyCode == "65") 
+    alert("The 'A' key was pressed")
+})
+
+// WHEEL SECTION
+  
